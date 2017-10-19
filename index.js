f76a60f6a2cc53be692c6299c83449825da97d81
Dhruv Hegde <desidhruv@gmail.com>
2017-10-19 02:38:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ let recipes = {};
 function updateObjectWithKeyAndValue(object, key, value){
 let obj = Object.assign({}, object);
 obj.key = value;
+return obj;
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){ 
+object.key = value; 
+return object; 
+}
+function deleteFromObjectbyKey(object, key){ 
+let obj = Object.assign({}, object); 
+delete obj[key]; 
 return obj; 
 }
-
+function destructivelyDeleteFromObjectByKey(object, key){ 
+delete object[key]; 
+return object; 
+}
