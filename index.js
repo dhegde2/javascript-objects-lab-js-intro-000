adca6e148465104b69c939ecd014154306f69421
Dhruv Hegde <desidhruv@gmail.com>
2017-10-19 02:35:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let recipes = {};
 function updateObjectWithKeyAndValue(object, key, value){
-let obj = Object.assign({}, object); 
-obj.key = value; 
+let obj = Object.assign({}, object);
+obj.key = value;
 return obj; 
 }
-}
+
