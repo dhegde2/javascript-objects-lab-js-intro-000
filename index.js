4bdc8d897c22f16e0ffed1cc082a2463c96ede3f
Dhruv Hegde <desidhruv@gmail.com>
2017-10-19 02:38:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ let obj = Object.assign({}, object);
 obj.key = value;
 return obj;
 }
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){ 
-object.key = value; 
-return object; 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+object.key = value;
+return object;
 }
-function deleteFromObjectbyKey(object, key){ 
-let obj = Object.assign({}, object); 
-delete obj[key]; 
-return obj; 
+function deleteFromObjectbyKey(object, key){
+let obj = Object.assign({}, object);
+delete obj[key];
+return obj;
 }
-function destructivelyDeleteFromObjectByKey(object, key){ 
-delete object[key]; 
-return object; 
+function destructivelyDeleteFromObjectByKey(object, key){
+delete object[key];
+return object;
 }
