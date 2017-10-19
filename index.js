0baa0015feebae8d58277038f9520487478b058a
Dhruv Hegde <desidhruv@gmail.com>
2017-10-19 02:43:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 let recipes = {};
-function updateObjectWithKeyAndValue(object, key, value){
+function updateObjectWithKeyAndValue(object,key,value){
 let copy = Object.assign({}, object);
 copy.key = value;
 return obj;
 }
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-object.key = 'value';
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+object.key = value;
 return object;
 }
-function deleteFromObjectbyKey(object, key){
+function deleteFromObjectbyKey(object,key){
 let obj = Object.assign({}, object);
 delete obj[key];
 return obj;
 }
-function destructivelyDeleteFromObjectByKey(object, key){
+function destructivelyDeleteFromObjectByKey(object,key){
 delete object[key];
 return object;
 }
